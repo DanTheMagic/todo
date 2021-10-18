15b55b6a30060f2e703f6b2bbb489526041dac4e
Daniel Frick <daniel.frick@hotmail.ch>
2021-10-18 16:26:51 +0200

    Insert simple todo into db

--- app/src/main/java/ch/ost/mge/todo/activities/TodoEditActivity.java
+++ app/src/main/java/ch/ost/mge/todo/activities/TodoEditActivity.java
@@ -1,16 +1,52 @@
 package ch.ost.mge.todo.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import ch.ost.mge.todo.R;
+import ch.ost.mge.todo.database.Todo;
+import ch.ost.mge.todo.database.TodoDatabase;
 
 public class TodoEditActivity extends AppCompatActivity {
+    private EditText titleEditText;
+    private EditText textEditText;
+    private Button saveButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_edit);
+
+        titleEditText = findViewById(R.id.title_edittext);
+        textEditText = findViewById(R.id.text_edittext);
+
+        saveButton = findViewById(R.id.save_button);
+        saveButton.setOnClickListener(v -> saveTodo());
+    }
+
+    private void saveTodo() {
+        String title = titleEditText.getText().toString();
+        String text = textEditText.getText().toString();
+
+        Todo todo = new Todo();
+        todo.title = title;
+        todo.text = text;
+
+        Runnable addToDb = () -> {
+            TodoDatabase db = Room.databaseBuilder(this, TodoDatabase.class, "todo.db").build();
+            db.todoDao().insert(todo);
+            db.close();
+
+            Intent intent = new Intent(this, TodoListActivity.class);
+            startActivity(intent);
+        };
+
+        new Thread(addToDb).start();
     }
 }
